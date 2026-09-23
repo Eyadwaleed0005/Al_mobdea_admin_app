6d857e998f0afbd7a794f9514a9de745be700c15
Mahmoudelnagar5 <elnagarm852@gmail.com>
2026-09-23 18:56:52 +0300

    feat(grades): implement grades data & domain

--- lib/app/dependency_injection/service_locator.dart
+++ lib/app/dependency_injection/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:al_mobdea_admin/app/dependency_injection/app_dependencies/app_de
 import 'package:al_mobdea_admin/app/dependency_injection/app_dependencies/core_dependencies.dart';
 import 'package:al_mobdea_admin/app/dependency_injection/features/dashboard_injection.dart';
 import 'package:al_mobdea_admin/app/dependency_injection/features/exams_dependencies.dart';
+import 'package:al_mobdea_admin/app/dependency_injection/features/grades_injection.dart';
 import 'package:al_mobdea_admin/app/dependency_injection/features/lesson_exams_dependencies.dart';
 import 'package:al_mobdea_admin/app/dependency_injection/features/live_session_injection.dart';
 import 'package:al_mobdea_admin/app/dependency_injection/features/lessons_dependencies.dart';
@@ -22,4 +23,5 @@ void setupServiceLocator() {
   registerStudyNotesDependencies(getIt);
   registerExamsDependencies(getIt);
   registerLiveSessionDependencies(getIt);
+  registerGradesDependencies(getIt);
 }
